5f0ee37ee45290af756b7758d2addc20d47c4108
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-06-24 22:15:01 +0100

    initialize offchain-java

---
+++ offchain-java/src/main/java/com/emia/offchainjava/OffChainJavaApplication.java
@@ -0,0 +1,13 @@
+package com.emia.offchainjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OffChainJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OffChainJavaApplication.class, args);
+    }
+
+}
